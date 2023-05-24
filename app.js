0a744be4e4089152acf6d47e6575bf9ddbd9e57d
Your Name <honagchuong050203@example.com>
2023-05-24 16:46:37 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// var btnopen = document.querySelector('.login-but')
+// var form = document.querySelector('.login')
+// var cancel = document.querySelector('.close')
+// document.getElementsByClassName(login-but).on
+// function toggleform()
+//
+    // document.getElementsByClassName(hide).
+    // form.classList.toggle('hide')
+//}
+// var login = document.querySelector('Register_but')
